5c988ff474ba5375b80611dfbb6dbf86bec51c64
Mark Marr <iam@markmarr.net>
2016-01-31 21:31:59 -0800

    First commit.

---
+++ tut3.py
@@ -0,0 +1,5 @@
+print("Welcome to Multiplier!")
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+result = num1*num2
+print(num1," x ",num2," = ",result)
